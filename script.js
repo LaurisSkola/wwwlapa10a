206bf095fa582c1b04f2b68c685d0c2a8ecba97e
LaurisSkola <lauris.ludbarzs@edu.kekava.lv>
2026-01-20 10:20:45 +0200

    viss
    azija

---
+++ script.js
@@ -0,0 +1,12 @@
+function poga(){
+    window.location.href="azija.html"
+}
+function poga1(){
+    window.location.href="eiropa.html"
+}
+function poga2(){
+    window.location.href="amerika.html"
+}
+function poga3(){
+    window.location.href="index.html"
+}
